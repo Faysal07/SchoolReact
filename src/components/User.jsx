5c52d9ecf5fd7624c27bddc80583c469d2de4dde
Faysal07 <faysalraktimurlmain@gmail.com>
2026-02-28 23:09:42 +0600

    user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,23 +5,36 @@ import { useEffect, useState } from "react";
 function User() {
 
     const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // API Calling Here
         fetch("https://jsonplaceholder.typicode.com/users")
         .then((res) => res.json())
-        .then((data) => setUsers(data))
+        .then((data) => {
+            setUsers(data)
+            // setLoading(false)
+
+            setTimeout(() => {
+                setLoading(false)
+            }, 3000);
+        })
 
         console.log("User Data", users)
     }, [])
 
+
+    if (loading) {
+        return(<h1>Please wait. Loading Data</h1>)
+    }
+
     return (
-        <div>
+        <div className='users'>
             <h2 className="">User Information</h2>
             {users.map(user => (
-                <div key={user.id}>
-                    <h3>User Name: {user.name}</h3>
-                    <h3>User Email: {user.email}</h3>
+                <div key={user.id} className='user_box'>
+                    <h3 className='user_title'>User Name: {user.name}</h3>
+                    <h3 className='user_email'>User Email: {user.email}</h3>
                 </div>
             ))}
 
